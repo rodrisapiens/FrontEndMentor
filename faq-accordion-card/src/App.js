93c98587282abc7e938cb8ede4a16d13d3288633
rodrisapiens <rniveyroreguero@frba.utn.edu.ar>
2022-02-12 19:39:14 -0300

    just adding

---
+++ faq-accordion-card/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Faqcard from "./components/Faqcard";
+
+function App() 
+{
+  return (
+    <div className="App">
+      <Faqcard/>
+    </div>
+  );
+}
+
+export default App;
